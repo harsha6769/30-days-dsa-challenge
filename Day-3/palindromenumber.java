e3978c65949f90f0291f13cb656f29e8fdd4294a
harsha6769 <harshavardhanreddygopireddy1@gmail.com>
2025-08-22 19:52:57 +0530

    Day 3 solutions added

---
+++ Day-3/palindromenumber.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static boolean isPalindrome(int x) {
+        if (x < 0) return false;
+        String s = String.valueOf(x);
+        int i = 0, j = s.length() - 1;
+        while (i < j) {
+            if (s.charAt(i) != s.charAt(j)) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println(isPalindrome(x));
+        sc.close();
+    }
+}
