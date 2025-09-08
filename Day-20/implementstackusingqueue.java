06ba408a5831c0606fe3dfe640b8a078493ecb3f
harsha6769 <harshavardhanreddygopireddy1@gmail.com>
2025-09-08 18:13:13 +0530

    day20

---
+++ Day-20/implementstackusingqueue.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class MyStack {
+    Queue<Integer> q1, q2;
+
+    public MyStack() {
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
+    }
+
+    public void push(int x) {
+        q2.add(x);
+        while (!q1.isEmpty()) {
+            q2.add(q1.remove());
+        }
+        Queue<Integer> temp = q1;
+        q1 = q2;
+        q2 = temp;
+    }
+
+    public int pop() {
+        return q1.remove();
+    }
+
+    public int top() {
+        return q1.peek();
+    }
+
+    public boolean empty() {
+        return q1.isEmpty();
+    }
+}
+
+public class Main225 {
+    public static void main(String[] args) {
+        MyStack stack = new MyStack();
+        stack.push(1);
+        stack.push(2);
+        System.out.println(stack.top());   // 2
+        System.out.println(stack.pop());   // 2
+        System.out.println(stack.empty()); // false
+    }
+}
---
+++ Day-20/numberofrecentcalls.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class RecentCounter {
+    Queue<Integer> queue;
+
+    public RecentCounter() {
+        queue = new LinkedList<>();
+    }
+
+    public int ping(int t) {
+        queue.add(t);
+        while (queue.peek() < t - 3000) {
+            queue.poll();
+        }
+        return queue.size();
+    }
+}
+
+public class Main933 {
+    public static void main(String[] args) {
+        RecentCounter obj = new RecentCounter();
+        System.out.println(obj.ping(1));    // 1
+        System.out.println(obj.ping(100));  // 2
+        System.out.println(obj.ping(3001)); // 3
+        System.out.println(obj.ping(3002)); // 3
+    }
+}
